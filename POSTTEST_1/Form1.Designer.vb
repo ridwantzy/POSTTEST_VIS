c5b13ab92bc31abbcf4322adf6856b584b17dcf8
ridwantzy <r1dw4n230806@gmail.com>
2026-02-28 17:25:45 +0800

    POSTTEST 1 - Kalkulator IPK Sederhana

--- POSTTEST_1/Form1.Designer.vb
+++ POSTTEST_1/Form1.Designer.vb
@@ -22,10 +22,97 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        btnTambah = New Button()
+        btnReset = New Button()
+        txtIPS = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        txtIPK = New TextBox()
+        predikat = New Label()
+        SuspendLayout()
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(310, 188)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 0
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(410, 188)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 1
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' txtIPS
+        ' 
+        txtIPS.Location = New Point(310, 143)
+        txtIPS.Name = "txtIPS"
+        txtIPS.Size = New Size(125, 27)
+        txtIPS.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(179, 146)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(86, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "IP Semester"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(179, 241)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(89, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "IP Kumulatif"
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.Location = New Point(310, 238)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(125, 27)
+        txtIPK.TabIndex = 5
+        ' 
+        ' predikat
+        ' 
+        predikat.AutoSize = True
+        predikat.Location = New Point(324, 288)
+        predikat.Name = "predikat"
+        predikat.Size = New Size(0, 20)
+        predikat.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(predikat)
+        Controls.Add(txtIPK)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtIPS)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents predikat As Label
 
 End Class
--- POSTTEST_1/Form1.vb
+++ POSTTEST_1/Form1.vb
@@ -1,3 +1,62 @@
 ﻿Public Class Form1
 
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim ips As Double
+
+        If txtIPS.Text = "" Then
+            MessageBox.Show("IPS tidak boleh kosong!")
+        Else
+
+            If Double.TryParse(txtIPS.Text, ips) Then
+
+                If ips >= 0 And ips <= 4 Then
+
+                    totalIP = totalIP + ips
+                    jumlahSemester = jumlahSemester + 1
+
+                    Dim ipk As Double
+                    ipk = totalIP / jumlahSemester
+
+                    txtIPK.Text = ipk.ToString("0.00")
+
+                    If ipk >= 2 And ipk <= 2.75 Then
+                        predikat.Text = "Cukup"
+                    ElseIf ipk >= 2.76 And ipk <= 3 Then
+                        predikat.Text = "Memuaskan"
+                    ElseIf ipk > 3 Then
+                        predikat.Text = "Sangat Memuaskan"
+                    Else
+                        predikat.Text = "-"
+                    End If
+
+                    txtIPS.Text = ""
+
+                Else
+                    MessageBox.Show("IPS harus antara 0 sampai 4!")
+                End If
+
+            Else
+                MessageBox.Show("IPS harus berupa angka!")
+            End If
+
+        End If
+
+    End Sub
+
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        totalIP = 0
+        jumlahSemester = 0
+
+        txtIPS.Text = ""
+        txtIPK.Text = ""
+        predikat.Text = "-"
+
+    End Sub
+
 End Class
